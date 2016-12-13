e43e23ea7c1ef7d9e88ffb92dc130f9075bddc54
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-12-13 16:37:02 +0000

    sh has no source?

--- ci.sh
+++ ci.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash 
 wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
 bash Miniconda_latest.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
