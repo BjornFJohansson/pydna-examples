51d647a984cba861273535ea45e2b6150c5a0372
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-12-16 18:36:54 +0000

    fixed modules

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 rm Miniconda_latest.sh
 conda update -yq conda
 conda config --add channels BjornFJohansson
-conda create -q -y -n testenv python=3.5 pydna nbval pytest
+conda create -q -y -n testenv python=3.5 pydna nbval pytest lxml requests
 source activate testenv
 which python
 python --version
