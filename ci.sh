914157d740c17485732fd2542a723ec62642ef7c
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-05-15 12:35:28 +0100

    spelling error

--- ci.sh
+++ ci.sh
@@ -3,7 +3,6 @@ wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O
 bash Miniconda_latest.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 rm Miniconda_latest.sh
-
 conda config --set always_yes yes --set show_channel_urls yes
 conda update conda
 conda config --add channels BjornFJohansson
@@ -11,8 +10,5 @@ conda create -qy -n testenv python=3.6 nbval pytest lxml requests
 source activate testenv
 which python
 python --version
-
 conda install pydna
-#conda install -c BjornFJohansson/label/test pydna=2.0.0a3
-
 python run_test.py
